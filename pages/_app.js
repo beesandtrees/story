ad6c5f414d4321a4020b3283d20b1f8de186d232
Melissa McMurtrie <melissa@melissakate.com>
2021-03-21 17:27:29 -0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import "../style.css";
+import '../style.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
